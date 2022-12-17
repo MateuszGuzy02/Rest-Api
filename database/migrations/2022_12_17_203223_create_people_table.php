9208b99f9fdd3edc43da3b942e55efc16e2dbeac
GuzyMateusz <guzymateusz02@gmail.com>
2022-12-17 23:04:31 +0100

    Create Database

---
+++ database/migrations/2022_12_17_203223_create_people_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
+            $table->string('telephone',255)->Unique();
+            $table->string('address', 255);
+            $table->string('city', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+};
